644372de1bc09199cd3b2fb8d97c0ce1bdb0f870
AixaCarrizo <aixa.carrizo1704@gmail.com>
2021-01-13 19:35:49 -0300

    lo unico que estoy segura que sirve es el buffer :v

---
+++ tp3/src/CPU_buffer.java
@@ -0,0 +1,26 @@
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+
+public class CPU_buffer {
+
+
+    private Queue<String> cola;
+
+    public CPU_buffer() {
+
+        cola = new ConcurrentLinkedQueue<>();
+    }
+
+    public void add(String dato) {
+        cola.add(dato);
+    }
+
+    public String remove() {
+        return cola.remove();
+    }
+
+    public  int size() {
+        return cola.size();
+    }
+}

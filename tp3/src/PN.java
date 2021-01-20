c18ea793f4bd474149c53972069618d8a86962e1
AixaCarrizo <aixa.carrizo1704@gmail.com>
2021-01-19 22:09:22 -0300

    odio la vida

--- tp3/src/PN.java
+++ tp3/src/PN.java
@@ -1,7 +1,7 @@
 public class PN {
 
-    int[]m;
-    int[]B;
+    int[] m;
+    int[] B;
     int[][] I;
     int[][] H;
     int[] Q;
@@ -9,7 +9,7 @@ public class PN {
 
 
     // N = cant de estados , M = cant de transiciones
-    public PN(int[] m, int[] b, int[] q, int[][] i, int[][] h, int [] E) {
+    public PN(int[] m, int[] b, int[] q, int[][] i, int[][] h, int[] E) {
         this.m = m; // Vector de marcado inicial // (N x 1)
         this.B = b; // Si B[i] = 1, la transicion esta desensibilizada (M x 1)
         this.Q = q; // Si M[i] = 0 -> Q[i] = 1 ; Caso contrario Q[i] = 0 (N x 1)
@@ -18,7 +18,7 @@ public class PN {
 
     }
 
-    public  PN(){
+    public PN() {
         init();
     }
 
@@ -38,7 +38,6 @@ public class PN {
          * m8: Stand_by
          */
 
-        this.S = new int[]{0, 0, 0, 0, 0, 0, 0, 0}; //Vector de disparo
 
         /**
          *
@@ -53,82 +52,92 @@ public class PN {
          *
          */
 
-        this.I = new int[][]{{0, 0, 0, 0, -1, 1, 0, 0, 0},        // arrival_rate
-                            {0, 0, -1, 0, 0, 0, 0, 0, 1},        // power_down_threshold
-                            {0, 1, 0, 0, 1, -1, 1, 0, 0},        // T1
-                            {1, -1, 0, -1, 0, 0, 0, 0, 0},        // T2
-                            {-1, 0, 0, 1, 0, 0, 0, 0, 0},        // T3
-                            {0, 0, 0, 0, 0, 0, -1, 0, 0},        // T5
-                            {0, 0, 0, 0, 0, 0, 0, 1, -1},        // T6
-                            {0, 0, 1, 0, 0, 0, -1, -1, 0},        // T7
+
+        this.I = new int[][]{{0, 0, 0, 1, -1, 0, 0, 0},        // m0
+                {0, 0, 1, -1, 0, 0, 0, 0},        // m1
+                {0, -1, 0, 0, 0, 0, 0, 1},        // m2
+                {0, 0, 0, -1, 1, 0, 0, 0},        // m3
+                {-1, 0, 1, 0, 0, 0, 0, 0},        // m4
+                {1, 0, -1, 0, 0, 0, 0, 0},        // m5
+                {0, 0, 1, 0, 0, -1, 0, -1},        // m6
+                {0, 0, 0, 0, 0, 0, 1, -1},        // m7
+                {0, 1, 0, 0, 0, 0, 1, 0},       // m8
         };
+
+
         this.H = new int[][]{{0, 1, 0, 0, 0, 0, 0, 0, 0},        // arrival_rate
-                            {0, 1, 0, 0, 0, 0, 0, 0, 0},        // power_down_threshold
-                            {0, 0, 0, 0, 0, 0, 0, 0, 0},        // T1
-                            {0, 0, 0, 0, 0, 0, 0, 0, 0},        // T2
-                            {0, 0, 0, 0, 0, 0, 0, 0, 0},        // T3
-                            {0, 0, 0, 0, 0, 0, 0, 0, 0},        // T5
-                            {0, 0, 0, 0, 0, 0, 0, 0, 0},        // T6
-                            {0, 0, 0, 0, 0, 0, 0, 0, 0},        // T7
+                {0, 1, 0, 0, 0, 0, 0, 0, 0},        // power_down_threshold
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},        // T1
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},        // T2
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},        // T3
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},        // T5
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},        // T6
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},        // T7
         };
 
         this.B = new int[]{0, 0, 0, 0, 0, 0, 0, 0}; //vector de transiciones desensibilizadas por arco inhibidor
         this.E = new int[]{0, 0, 0, 0, 0, 0, 0, 0}; //vector de sensibilizado
 
 
-
     }
 
 
-    public boolean isPos(int [] index) {
+    public boolean isPos(int[] index) {
 
-        for (int i = 0; i < 9; i++){ //M(pi) = 0 -> Q[i] = 1, M(pi) != 0 -> Q[i] = 0
+        for (int i = 0; i < 9; i++) { //M(pi) = 0 -> Q[i] = 1, M(pi) != 0 -> Q[i] = 0
             if (m[i] != 0) Q[i] = 0;
             else Q[i] = 1;
         }
 
 
+        //calculo E
+
+        for (int i = 0; i < 8; i++) {
+            this.E[i] = 1;
+            for (int j = 0; j < 9; j++) {
+                if (I[j][i] + m[j] < 0) {
+                    E[i] = 0;
+                    break;
+                }
+            }
+        }
+
+
         int temp = 0;
-        int [] aux = new int[] {0, 0, 0, 0, 0, 0, 0, 0};
+        int[] aux = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
         for (int j = 0; j < 8; j++) {
             B[j] = 0;
-            E[j] = 0;
-
             for (int i = 0; i < 9; i++) {   //Si algun numero del nuevo vector de marcado es = 1, no puedo dispararla
                 temp = H[j][i] * Q[i];    //Sumo para obtener el nuevo vector de desensibilizado
                 B[j] = B[j] + temp; // B = 0 -> no se puede :(
-
-                temp = I[j][i] * m[i];    //Sumo para obtener el nuevo vector de desensibilizado
-                E[j] = E[j] + temp;         // E = 0 -> no se puede :(
             }
 
-            if( B[j] * E[j] > 0 ) aux[j] = 1; // B and E
+            if (B[j] * E[j] > 0) aux[j] = 1; // B and E
 
-            if(aux[j] * index[j] > 0) aux[j] = 1; // sigma and Ex
+            if (aux[j] * index[j] > 0) aux[j] = 1; // sigma and Ex
 
         }
 
+
+        // I * aux  (n x m * m x 1 = n x 1)
+        int[] aux2 = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
         temp = 0;
-        int [] aux2 = new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0};
-
-        for(int j = 0; j < 9; j++){
-            for (int i = 0; i < 8; i++){
-                temp = I[j][i]
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 8; j++) {
+                temp = I[i][j] * aux[j]
+                aux2[i] = aux2[i] + temp;
             }
         }
 
-
-        int[] mPrima = new int[9];
+        int[] mPrima = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
 
         for (int i = 0; i < 9; i++) {   //Si algun numero del nuevo vector de marcado es negativo, no puedo dispararla
-            mPrima[i] = m[i] + ();    //Sumo para obtener el nuevo vector de marcado
+            mPrima[i] = m[i] + aux2[i];    //Sumo para obtener el nuevo vector de marcado
 
             if (mPrima[i] == -1) return false;
+
         }
-
-
-
+        return true;
     }
-
 
 }

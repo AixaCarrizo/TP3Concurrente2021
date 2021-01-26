55d7378a8adc9dec65d164a957117299fa2fb839
BeluTrac <48957384+BeluTrac@users.noreply.github.com>
2021-01-26 19:47:54 -0300

    Agregue el disparo aleatorio en GenData y el segundo buffer para el otro nucleo.

--- tp3/src/GenData.java
+++ tp3/src/GenData.java
@@ -2,13 +2,15 @@ public class GenData extends Thread {
 
     private Monitor monitor;
     private CPU_buffer buffer;
+    private CPU_buffer buffer2;
     private int arrivalRate;
 
-    public GenData(Monitor monitor, CPU_buffer cpuBuffer,int arrivalRate)
+    public GenData(Monitor monitor, CPU_buffer cpuBuffer, CPU_buffer cpuBuffer2,int arrivalRate)
     {
     this.monitor = monitor;
     this.buffer = cpuBuffer;
     this.arrivalRate = arrivalRate;
+    this.buffer2=cpuBuffer2;
     }
 
   @Override
@@ -19,14 +21,28 @@ public class GenData extends Thread {
           int nroData = 0;
 
           while (true) {
+
+              double choose = Math.random()*100+1;
+
               monitor.shoot(0); //Disparo Arrival_rate
 
               Thread.sleep(arrivalRate);
 
-              monitor.shoot(2); //Disparo T1
-              buffer.add("Dato numero: " + nroData); //Agrego un elemento al buffer
-              System.out.println("Dato numero: " + nroData);
-              nroData++;
+              //Cambiar transiciones despues
+              if(choose<50)
+              {
+                  monitor.shoot(2); //Disparo T1
+                  buffer.add("Dato numero: " + nroData); //Agrego un elemento al buffer
+                  System.out.println("Dato numero: " + nroData);
+                  nroData++;
+              }else
+              {
+                  monitor.shoot(2); //
+                  buffer.add("Dato numero: " + nroData); //Agrego un elemento al buffer (Cambiar por buffer2)
+                  System.out.println("Dato numero: " + nroData);
+                  nroData++;
+              }
+
           }
       }catch(InterruptedException e)
       {
--- tp3/src/Main.java
+++ tp3/src/Main.java
@@ -8,11 +8,12 @@ public class Main {
     private final static Condition notFullBuffer1 = lock.newCondition();
     private final static Condition powerDownCpu1 = lock.newCondition();
     public static CPU_buffer buffer1= new CPU_buffer();
+    public static CPU_buffer buffer2= new CPU_buffer();
     private static Monitor monitor = new Monitor(buffer1, lock, notEmptyBuffer1, notFullBuffer1, powerDownCpu1);
 
     public static void main(String[] args) {
 
-        GenData gd = new GenData(monitor,buffer1,1000);
+        GenData gd = new GenData(monitor,buffer1,buffer2,1000);
         gd.run();
     }
 }

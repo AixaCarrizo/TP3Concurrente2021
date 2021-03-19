da21fd4a5d4dde5558f0556fbc8a21a5f7cf1e0a
AixaCarrizo <aixa.carrizo1704@gmail.com>
2021-03-18 23:07:17 -0300

    cambios menores en el script de los invariantes

--- test.py
+++ test.py
@@ -2,12 +2,13 @@
 import fileinput
 import re
 
-def regex2(file ):
+def verifyInvariants(file ):
 	i=0
 	line = [file.readline() , 0]
 	while(True):
 		
-		line = re.subn('T0(.*?)((TA(.*?)((T6(.*?)T7(.*?)T2(.*?)T3(.*?)T4)|(T5(.*?)T2(.*?)T3)))|((T8(.*?)((T13(.*?)T14(.*?)T9(.*?)T10(.*?)T11)|(T12(.*?)T9(.*?)T10)))))','\g<1>\g<4>\g<7>\g<8>\g<9>\g<10>\g<12>\g<13>\g<16>\g<19>\g<20>\g<21>\g<22>\g<24>\g<25>', line[0].rstrip())
+		line = re.subn('T0(.*?)((TA(.*?)((T6(.*?)T7(.*?)T2(.*?)T3(.*?)T4)|(T5(.*?)T2(.*?)T3)))|((T8(.*?)((T13(.*?)T14(.*?)T9(.*?)T10(.*?)T11)|(T12(.*?)T9(.*?)T10)))))',
+		'\g<1>\g<4>\g<7>\g<8>\g<9>\g<10>\g<12>\g<13>\g<16>\g<19>\g<20>\g<21>\g<22>\g<24>\g<25>', line[0].rstrip())
 		#print(line[0])
         
 		i = i+1
@@ -27,5 +28,5 @@ def regex2(file ):
 file = open("prueba.txt" , "r")
 
 print("Test de invariantes de transicion:")
-regex2(file)
+verifyInvariants(file)
 

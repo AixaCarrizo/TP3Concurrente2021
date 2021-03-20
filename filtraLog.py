09f95754ad84e7f1373898140302b8ba7d669cf8
BeluTrac <48957384+BeluTrac@users.noreply.github.com>
2021-03-19 21:06:48 -0300

    Se arreglo el archivo en python

--- filtraLog.py
+++ filtraLog.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import re
 buffer1 = []
 buffer2 = []
@@ -13,13 +14,26 @@ for i in open("log.txt","r"):
      aux2 = aux[(aux.find("tiene ")+6):]
      buffer2.append(aux2[:aux2.index(" eleme")])
 
-plt.plot(buffer1,'b-',buffer2,'r-')
-plt.ylabel('Cantidad en el buffer')
-plt.xlabel('Numero de muestra')
-plt.yscale('linear')
-plt.show()
-
 archivo = open("log2.txt","w")
 archivo.write(" ".join(buffer1))
 archivo.write("\n\n")
 archivo.write(" ".join(buffer2))
+
+
+     
+buffer1 = list(map(lambda x: int(x),buffer1))
+buffer2 = list(map(lambda x: int(x),buffer2))
+
+
+x = np.arange(0., len(buffer1), 1.)
+plt.plot(x,buffer1,'b-')
+plt.plot(x,buffer2,'r-')
+plt.ylabel('Cantidad en el buffer')
+plt.xlabel('Numero de muestra')
+plt.yscale('linear')
+plt.grid(True, which='both')
+plt.axhline(y=0, color='k')
+plt.axvline(x=0, color='k')
+plt.show()
+
+

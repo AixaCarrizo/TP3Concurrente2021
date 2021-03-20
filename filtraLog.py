ed66eb6fc4942b61468842f273b3da3bcee5cd0f
Juan Ignacio Fernandez <jruloignacio@hotmail.com>
2021-03-19 21:40:58 -0300

    Se modifico filtraLog.py, se ignoro los .txt y se eliminaron log2 y logsimple

--- filtraLog.py
+++ filtraLog.py
@@ -13,27 +13,18 @@ for i in open("log.txt","r"):
      buffer1.append(aux[:aux.index(" elementos")])
      aux2 = aux[(aux.find("tiene ")+6):]
      buffer2.append(aux2[:aux2.index(" eleme")])
-
-archivo = open("log2.txt","w")
-archivo.write(" ".join(buffer1))
-archivo.write("\n\n")
-archivo.write(" ".join(buffer2))
-
-
      
 buffer1 = list(map(lambda x: int(x),buffer1))
 buffer2 = list(map(lambda x: int(x),buffer2))
 
 
-x = np.arange(0., len(buffer1), 1.)
-plt.plot(x,buffer1,'b-')
-plt.plot(x,buffer2,'r-')
+plt.plot(buffer1,'b-', lw="0.5", label="Buffer1")
+plt.plot(buffer2,'r-', lw="0.5", label="Buffer2")
 plt.ylabel('Cantidad en el buffer')
 plt.xlabel('Numero de muestra')
 plt.yscale('linear')
+plt.legend(loc="upper right")
 plt.grid(True, which='both')
 plt.axhline(y=0, color='k')
 plt.axvline(x=0, color='k')
 plt.show()
-
-
